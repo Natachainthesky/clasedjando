e673edba66346364242762a5314bd14fd16984d1
NatachaBaffo <natachabaffo@gmail.com>
2022-07-05 16:49:13 -0300

    agregué AppDesafio. cree modelo con clase Familiar

--- MVTNatachaBaffo/AppDesafio/models.py
+++ MVTNatachaBaffo/AppDesafio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Familiar (models.Model):
+    nombre=models.CharField()
+    edad=models.IntegerField()
+    fecha_nac=models.DateField()
+    
