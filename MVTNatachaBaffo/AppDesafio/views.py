cffcf57b9f44486ea6847ab2fd444c76ffabdb18
NatachaBaffo <natachabaffo@gmail.com>
2022-07-05 17:39:23 -0300

    agregué desde viws un familiar y el path a url

--- MVTNatachaBaffo/AppDesafio/views.py
+++ MVTNatachaBaffo/AppDesafio/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from models import Familiar
+from django.http import HttpResponse
 
 # Create your views here.
+def familiar(self):
+    familiar= Familiar(nombre="Malena", edad=23, fecha_nac='%d26-%m02-%y99') 
+    familiar.save()
+    texto= f'Nombre: {familiar.nombre} Edad: {familiar.edad} Fecha de Nacimiento: {familiar.fecha_nac}'
+
+    return HttpResponse(texto)
