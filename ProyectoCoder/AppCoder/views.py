80235c02121ac2c7803b33b8c3156e21dbc1177e
NatachaBaffo <natachabaffo@gmail.com>
2022-07-06 10:30:23 -0300

    agregamos bootstrap

--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -1,3 +1,4 @@
+from cgitb import html
 from django.shortcuts import render
 from AppCoder.models import Curso
 from django.http import HttpResponse
@@ -8,3 +9,15 @@ def curso(self):
     curso.save()
     texto= f'Curso creado. {curso.nombre} {curso.comision}'
     return HttpResponse(texto)
+
+def inicio (request):
+    return render(request, 'AppCoder/inicio.html')
+
+def estudiantes (request):
+    return render(request, 'AppCoder/estudiantes.html')
+
+def  profesores (request):
+    return render(request, 'AppCoder/profesores.html')
+
+def entregables (request):
+    return render(request, 'AppCoder/entregables.html')
